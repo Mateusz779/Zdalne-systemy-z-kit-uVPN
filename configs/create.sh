b4032ed40f11bcc733e16f6d0e926a0a5fa6a1fa
server <server@praktyki>
2023-04-13 06:54:28 -0400

    script

--- configs/create.sh
+++ configs/create.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-usage() { echo "Usage: [-i <ini config>] [-c <conf file>] [-k <pub server key>] [-l <priv key lenght>] [-n <name>] [-s <dir with scripts>]" 1>&2; exit 1; }
+kitcrypto_version="0.0.3"
+uvpn3_version="3.0.3"
 
-while getopts "i:c:k:l:n:s:" option
-do 
+usage() { echo "Usage: [-a <root ssh authorized_keys>] [-b add executable to output] [-c <conf file>] [-d <sshd_config>] [-i <ini config>] [-k <pub server key>] [-l <priv key lenght>] [-m <msmtp script>] [-n <name>] [-s <dir with scripts>]" 1>&2; exit 1; }
+
+while getopts "a:b:c:d:e:i:k:l:m:n:s:" option
+do
     case "${option}"
         in
+	a)akeys=${OPTARG};;
+	b)build="yes";;
+	c)conf=${OPTARG};;
+	d)sshconf=${OPTARG};;
         i)ini=${OPTARG};;
-        c)conf=${OPTARG};;
         k)key=${OPTARG};;
 	l)keylen=${OPTARG};;
+	m)msmtp=${OPTARG};;
 	n)name=${OPTARG};;
 	s)scripts=${OPTARG};;
 	*)usage;;
@@ -20,17 +27,18 @@ shift $((OPTIND-1))
 CONFIGS=$(pwd)
 echo "${CONFIGS}"
 
+
 sudo apt update
 sudo apt install cmake make g++ gcc libssl-dev libgmp-dev
 
 cd /tmp
-wget -O uVPN.tar.xz https://opensource.krypto-it.pl/uVPN/uVPN-3.0.3.tar.xz
+wget -O uVPN.tar.xz https://opensource.krypto-it.pl/uVPN/uVPN-$uvpn3_version.tar.xz
 mkdir uVPN
 tar -xvf uVPN.tar.xz -C uVPN
 rm uVPN.tar.xz
 mv uVPN/*/* uVPN/
 
-wget -O kit-crypto.tar.xz https://opensource.krypto-it.pl/kit-crypto-c/kit-crypto-c-0.0.2.tar.xz
+wget -O kit-crypto.tar.xz https://opensource.krypto-it.pl/kit-crypto-c/kit-crypto-c-$kitcrypto_version.tar.xz
 mkdir kit-crypto
 tar -xvf kit-crypto.tar.xz -C kit-crypto
 rm kit-crypto.tar.xz
@@ -44,30 +52,49 @@ cd ../uVPN.bin
 cmake ../uVPN -DKIT_CRYPTO_INCLUDES=/tmp/kit-crypto/include -DKIT_CRYPTO_LIB=/tmp/kit-crypto/libkitcryptoc_static.a
 make
 mkdir /tmp/output
-echo $keylen
-./uVPN_rsagen $keylen > /tmp/output/uVPN.priv
-head -2 /tmp/output/uVPN.priv > /tmp/output/$name.pub
+mkdir /tmp/output/vpn
+mkdir /tmp/output/ssh
+./uVPN_rsagen $keylen > /tmp/output/vpn/uVPN.priv
+head -2 /tmp/output/vpn/uVPN.priv > /tmp/output/vpn/$name.pub
 
-mv uVPN3 /tmp/output
-cd $CONFIGS
-cp $conf /tmp/output
-cp $ini /tmp/output
-cp $key /tmp/output
-if [ -n "$scripts" ]; then
-  cp -r $scripts /tmp/output/
+if [ -n "$build" ]; then
+  mv uVPN3 /tmp/output/vpn
 fi
-sed -i '/^private_key/c\private_key uVPN.priv' /tmp/output/$conf
-sed -i '/^name/c\name '"$name" /tmp/output/$conf
-sed -i '/^servers_config/c\servers_config '"$ini" /tmp/output/$conf
-sed -i '1s/.*/['"$name"']/' /tmp/output/$ini
+
+cd $CONFIGS
+cp $conf /tmp/output/vpn
+cp $ini /tmp/output/vpn
+cp $key /tmp/output/vpn
+if [ -n "$scripts" ]; then
+  cp -r $scripts /tmp/output/vpn
+fi
+
+if [ -n "$akeys" ]; then
+  cp  $akeys /tmp/output/ssh
+fi
+
+if [ -n "$sshconf" ]; then
+  cp  $sshconf /tmp/output/ssh
+fi
+
+mkdir /tmp/output/msmtp
+if [ -n "$msmtp" ]; then
+  cp  $msmtp /tmp/output/msmtp
+fi
+
+
+sed -i '/^private_key/c\private_key uVPN.priv' /tmp/output/vpn/$conf
+sed -i '/^name/c\name '"$name" /tmp/output/vpn/$conf
+sed -i '/^servers_config/c\servers_config '"$ini" /tmp/output/vpn/$conf
+sed -i '1s/.*/['"$name"']/' /tmp/output/vpn/$ini
 
 cd /tmp/output
-mkdir vpn
-mv * vpn
+mkdir configs
+mv * configs
 mkdir -t $CONFIGS/squash
 mkdir -t $CONFIGS/squash/$name
 mksquashfs . $CONFIGS/squash/$name.squashfs
-cp /tmp/output/vpn/$name.pub $CONFIGS/squash/$name
+cp /tmp/output/configs/vpn/$name.pub $CONFIGS/squash/$name.pub
 
 rm -rf /tmp/kit-crypto
 rm -rf /tmp/uVPN*
