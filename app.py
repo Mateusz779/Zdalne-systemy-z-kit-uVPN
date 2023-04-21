ebd558a6905431e266caba75ab9be9b7a50faffe
Mateusz779 <mateusz.kedziora@mkedziora.pl>
2023-04-21 11:02:06 +0200

    -

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main():
         if db.get_user_bytoken(auth_token) is None:
             return redirect("/login")
     machines_all = db.get_machines()
-    return render_template('index.html', ssh_port=config.webssh_port, machines=machines_all.machines, timezone=config.timezone)
+    return render_template('index.html', ssh_port=config.webssh_port, machines=machines_all.machines)
 
 
 @app.route('/login')
@@ -33,7 +33,7 @@ def login():
     if auth_token != "" or auth_token is not None:
         if db.get_user_bytoken(auth_token) is not None:
             machines_all = db.get_machines()
-            return render_template('index.html', ssh_port=config.webssh_port, machines=machines_all.machines, timezone=config.timezone)
+            return render_template('index.html', ssh_port=config.webssh_port, machines=machines_all.machines)
     return render_template('login.html')
 
 
