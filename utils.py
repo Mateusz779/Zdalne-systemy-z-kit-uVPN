9b62de71799a54d524dc119b9cbc0c754806e97e
Mateusz779 <mateusz.kedziora@mkedziora.pl>
2023-04-25 10:15:28 +0200

    -

--- utils.py
+++ utils.py
@@ -44,10 +44,11 @@ def ssh_thread_function():
     except:
         pass
     if os.path.exists(os.path.join(os.getcwd(), 'keys', "sshkey")) is False:
-        subprocess.run(['ssh-keygen', '-t dsa', '-f '+os.path.join(os.getcwd(), 'keys', "sshkey"), '-P ""'])
+        subprocess.run(['ssh-keygen', '-t dsa', '-f ' +
+                       os.path.join(os.getcwd(), 'keys', "sshkey"), '-P ""'])
     while os.path.exists(os.path.join(os.getcwd(), 'keys', "sshkey")) is False:
         pass
-    subprocess.run(['wssh', '--fbidhttp=False', '--port='+config.webssh_port, 
+    subprocess.run(['wssh', '--fbidhttp=False', '--port='+config.webssh_port,
                     '--hostfile='+os.path.join(os.getcwd(), 'keys', "sshkey")])
 
 
