247dc89c97ae7826e6972b6c654bd4ade9881f96
Mateusz779 <mateusz.kedziora@mkedziora.pl>
2023-04-25 10:11:16 +0200

    -

--- utils.py
+++ utils.py
@@ -43,11 +43,8 @@ def ssh_thread_function():
         os.mkdir(os.path.join(os.getcwd(), 'keys'))
     except:
         pass
-    print("cycki")
-    print(os.path.exists(os.path.join(os.getcwd(), 'keys', "sshkey")))
     if os.path.exists(os.path.join(os.getcwd(), 'keys', "sshkey")) is False:
-        print(os.path.join(os.getcwd(), 'keys', "sshkey"))
-        subprocess.run(['ssh-keygen', '-t rsa ', '-f '+os.path.join(os.getcwd(), 'keys', "sshkey"), '-P ""'])
+        subprocess.run(['ssh-keygen', '-trsa ', '-f '+os.path.join(os.getcwd(), 'keys', "sshkey"), '-P ""'])
     while os.path.exists(os.path.join(os.getcwd(), 'keys', "sshkey")) is False:
         pass
     subprocess.run(['wssh', '--fbidhttp=False', '--port='+config.webssh_port, 
